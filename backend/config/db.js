50b68d29260fcde350c129f3f91eb81a5e4b8f81
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2022-12-01 23:11:47 +0530

    increase SQL time out

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv').config()
 var mysql      = require('mysql');
 
 const db  = mysql.createPool({
-    connectionLimit: 520,
+    connectionLimit: 30000,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
